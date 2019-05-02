37509654a235416a1ec238f44b83f69403722a8c
Fufangzhou <316585692@qq.com>
2019-05-02 19:44:02 +0800

    v0.1.14 修改了注册信息第三步的接口逻辑，防止重复注册

--- EDARootServer/controller/usercontroller/signupinfo.js
+++ EDARootServer/controller/usercontroller/signupinfo.js
@@ -16,36 +16,43 @@ module.exports = async function (ctx, next) {
         if (userObj == null) {
             ctx.body = {
                 code: netCode.MAILNOTEXIST,
-                info: "请不要擅自再次修改邮箱信息"
+                info: "邮箱不存在，请不要擅自修改信息"
             }
         } else {
-            if (!util.emailCheck(userinfo.email)) {
-                ctx.body = {
-                    code: netCode.BASICINFOERR,
-                    info: "邮箱不符合规范"
-                }
-            } else if (!util.infoStringCheck(userinfo.name)) {
-                ctx.body = {
-                    code: netCode.BASICINFOERR,
-                    info: "用户的名称不能为空"
-                }
-            } else if (!util.infoNumberCheck(userinfo.grade)) {
-                ctx.body = {
-                    code: netCode.BASICINFOERR,
-                    info: "用户的年级信息有误"
+            if (userObj.status != userStatusCode.FINISHEDREGISTER) {
+                if (!util.emailCheck(userinfo.email)) {
+                    ctx.body = {
+                        code: netCode.BASICINFOERR,
+                        info: "邮箱不符合规范"
+                    }
+                } else if (!util.infoStringCheck(userinfo.name)) {
+                    ctx.body = {
+                        code: netCode.BASICINFOERR,
+                        info: "用户的名称不能为空"
+                    }
+                } else if (!util.infoNumberCheck(userinfo.grade)) {
+                    ctx.body = {
+                        code: netCode.BASICINFOERR,
+                        info: "用户的年级信息有误"
+                    }
+                } else {
+                    userObj.email = userinfo.email;
+                    userObj.name = userinfo.name;
+                    userObj.grade = userinfo.grade;
+                    userObj.info = userinfo.info;
+                    userObj.password = password;
+                    userObj.status = userStatusCode.FINISHEDREGISTER;
+                    ctx.session.email = userinfo.email;
+                    await userObj.save();
+                    ctx.body = {
+                        code: netCode.SUCCESS,
+                        info: "已经成功完成用户注册"
+                    }
                 }
             } else {
-                userObj.email = userinfo.email;
-                userObj.name = userinfo.name;
-                userObj.grade = userinfo.grade;
-                userObj.info = userinfo.info;
-                userObj.password = password;
-                userObj.status = userStatusCode.FINISHEDREGISTER;
-                ctx.session.email = userinfo.email;
-                await userObj.save();
                 ctx.body = {
-                    code: netCode.SUCCESS,
-                    info: "已经成功完成用户注册"
+                    code: netCode.MAILEXIST,
+                    info: "请注意你的邮箱是否已经被重复注册"
                 }
             }
         }
